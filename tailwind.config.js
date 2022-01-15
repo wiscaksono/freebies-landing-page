4d65e9190d5f30e9d7325251097af9669898ef9b
wiscaksono <wwicaksono96@gmail.com>
2022-01-15 22:04:04 +0700

    intial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F53855",
+        base: "#4F5665",
+        black: "#0B132A",
+      },
+    },
+  },
+  plugins: [],
+};
